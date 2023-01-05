d80a1eccb2fa529fcd453f98b12b1cb951e0d13a
jamesatmiami <beauprjl@miamioh.edu>
2023-01-05 16:18:14 -0500

    initial draft

--- forms.js
+++ forms.js
@@ -23,8 +23,11 @@ function initForms () {
     {selector: "output",   color: "teal",   label: "output"},
     {selector: "progress", color: "olive",  label: "progress"},
     {selector: "select",   color: "green",  label: "select"},
-    {selector: "textarea", color: "brown",  label: "textarea"}
-  ];
+    {selector: "textarea", color: "brown",  label: "textarea"},
+    {selector: "[role=combobox]",color: "navy",label: "combobox"}, 
+    {selector: "[role=button]", color: "purple", label: "button" },
+    { selector: "[role=textbox]", color: "navy", label: "input" }, 
+    { selector: "[role=checkbox]", color: "navy", label: "input" }];
 
   let selectors = targetList.map(function (tgt) {return '<li>' + tgt.selector + '</li>';}).join('');
 
