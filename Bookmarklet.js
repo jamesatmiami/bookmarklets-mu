032e0c7554d3f8ddf646e523671212336e4d6666
jamesatmiami <beauprjl@miamioh.edu>
2023-01-13 16:41:54 -0500

    more tooltip changes

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -50,7 +50,7 @@ Bookmarklet.prototype.run = function () {
     // Initializes tooltips
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl);
+      return new bootstrap.Tooltip(tooltipTriggerEl, {container: '.bootstrap-bm'});
     });
   console.log("Tooltips initialized");
   }
