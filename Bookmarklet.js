6acb7b64ed796e829171f710806814491ab2aa17
jamesatmiami <beauprjl@miamioh.edu>
2023-01-13 16:53:19 -0500

    test

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -50,7 +50,7 @@ Bookmarklet.prototype.run = function () {
     // Initializes tooltips
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl, {container: '.bootstrap-bm'});
+      return new bootstrap.Tooltip(tooltipTriggerEl, {selector: '.oaa-overlay-label'});
     });
   console.log("Tooltips initialized");
   }
